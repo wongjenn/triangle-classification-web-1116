f75bbcfd76e7b776534549244934ffb1a51154ac
wongjenn <wongjenn@yahoo.com>
2016-12-06 20:47:49 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if (side1 + side2 <= side3) || (side2 + side3 <= side1) || (side3 + side1 <= side2) || (side1 <= 0 || side2 <= 0 || side3 <= 0)
+        raise TriangleError
+    elsif side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side3 == side1
+      :isosceles
+    elsif side1 != side2 && side2 != side3 && side3 != side1
+      :scalene
+    end
+  end
 end
+
+  class TriangleError < StandardError
+    def message
+      "Triangle violating the triangle inequality are illegal."
+    end
+  end
